396008c3fdef2186719cc2c8a4af0d24eba53852
matt-gallery <matt.r.gallery@gmail.com>
2025-03-25 13:38:24 -0400

    afternoon lecture update.  CRUD complete

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -4,31 +4,62 @@ import Fruit from "../models/fruit.js";
 const fruitsRouter = Router();
 
 fruitsRouter.get("/", async (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 fruitsRouter.get("/fruits/new", (req, res) => {
-  res.render("fruits/new.ejs");
+  res.render("fruits/new");
 });
 
 fruitsRouter.get("/fruits", async (req, res) => {
   const fruits = await Fruit.find({});
-  res.render("fruits/index.ejs", { fruits });
+  res.render("fruits/index", { fruits });
 });
 
 fruitsRouter.post("/fruits", async (req, res) => {
   let { name, isReadyToEat } = req.body;
   if (isReadyToEat) {
-    isReadytoEat = true;
+    isReadyToEat = true;
   } else {
-    isReadyToEat
+    isReadyToEat;
   }
 
   const fruit = await Fruit.create({ name, isReadyToEat });
 
+  res.redirect("/fruits");
+});
 
-res.redirect("/fruits");
+fruitsRouter.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show", { fruit: foundFruit });
+});
 
+fruitsRouter.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+fruitsRouter.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+// server.js
+
+fruitsRouter.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
 });
 
 export default fruitsRouter;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import methodOverride from 'method-override';
 import "./db/connection.js"
 
 import fruitsRouter from './controllers/fruits.js';
@@ -9,6 +11,8 @@ const app = express();
 // Middleware
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
 
 // Routes
 app.use("/", fruitsRouter);
